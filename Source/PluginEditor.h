ca6941a7cc83506694a1d14f29339772567472ca
MACAVELLESWAG <thedinbot@gmail.com>
2026-05-13 00:24:55 -0500

    Clean minimal GUI version with bean background and LCD

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,10 +1,15 @@
 #pragma once
+
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include "LcdDisplay.h"
 
-class LYNEVIPluginEditor : public juce::AudioProcessorEditor {
+class LYNEVIPluginEditor : public juce::AudioProcessorEditor
+{
 public:
     LYNEVIPluginEditor(LYNEVIPluginProcessor&);
+    ~LYNEVIPluginEditor() override;
+
     void paint(juce::Graphics&) override;
     void resized() override;
 
@@ -12,10 +17,12 @@ private:
     LYNEVIPluginProcessor& audioProcessor;
     juce::Image backgroundImage;
 
-    // All knobs from the red bean reference
+    // Knobs
     juce::Slider driveKnob, bassKnob, middleKnob, trebleKnob, presenceKnob;
     juce::Slider chanVolKnob, reverbKnob, outputKnob;
     juce::Slider ampModelsKnob, effectsKnob;
+
+    std::unique_ptr<LcdDisplay> lcdDisplay;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(LYNEVIPluginEditor)
 };
